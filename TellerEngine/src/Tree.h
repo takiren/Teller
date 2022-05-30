8706696243ea29807ad83549c3a509a771991c1c
takiren <tyorokyuu@gmail.com>
2022-05-30 22:18:04 +0900

    なんかいろいろビルドしたりignore変更したり

--- TellerEngine/src/Tree.h
+++ TellerEngine/src/Tree.h
@@ -2,12 +2,16 @@
 #include<vector>
 #include<memory>
 #include<string>
+#include<map>
+#include<Eigen>
 
 #include"cinder/CinderImGui.h"
-
+#include"imgui_node_editor.h"
 #include"Episode.h"
 
 namespace Teller {
+	using namespace ci;
+	namespace ed = ax::NodeEditor;
 	enum class Node_TYPE {
 		BLANK,
 		EPISODE,
@@ -19,7 +23,8 @@ namespace Teller {
 		Delegate,
 		BOOL,
 		INT,
-		OPTION
+		OPTION,
+		TIMELINE
 	};
 
 	class SocketBase;
@@ -30,12 +35,12 @@ namespace Teller {
 		std::string description;
 		ImVec2 position, size;
 		Node_TYPE type;
-	public:
 		std::vector<std::shared_ptr<SocketBase>> inputSockets;
 		std::vector<std::shared_ptr<SocketBase>> outputSockets;
 		std::vector<std::shared_ptr<SocketBase>> sockets;
-		std::vector<std::shared_ptr<NodeBase>> children;
+	public:
 		std::vector<std::weak_ptr<NodeBase>> parents;
+		std::vector<std::shared_ptr<NodeBase>> children;
 
 		NodeBase(int key) :ID_(key) {};
 		~NodeBase() = default;
@@ -100,6 +105,7 @@ namespace Teller {
 
 	class SocketInt :public SocketBase {
 	private:
+	public:
 
 	};
 
@@ -119,5 +125,101 @@ namespace Teller {
 		void AddNode(const std::shared_ptr<NodeBase> node);
 		void RemoveNode(int key);
 	};
+
+	class TNodeCore;
+	class TSocketCore :public std::enable_shared_from_this<TSocketCore>{
+	protected:
+	public:
+		uint64_t ID_;
+		std::weak_ptr<TNodeCore> parentTNode;
+		std::vector <std::weak_ptr<TSocketCore>> target_;
+		Socket_TYPE type_;
+		TSocketCore() = delete;
+		TSocketCore(Socket_TYPE _type) :
+			type_(_type),
+			ID_((uint64_t)this)
+		{};
+	};
+
+
+	struct TSocketInput :TSocketCore {
+	};
+
+	struct TSocketOutput :TSocketCore {
+	};
+
+	//ノード管理クラス
+	class TNodeManager {
+		std::map<uint64_t,std::shared_ptr<TNodeCore>> nodes;
+		struct LinkInfo
+		{
+			ed::LinkId Id;
+			ed::PinId  InputId;
+			ed::PinId  OutputId;
+		};
+
+	public:
+		TNodeManager() = default;
+		void AddTNode(Node_TYPE _type);
+
+		std::vector<LinkInfo> MakeLinkVector();
+	};
+
+	template<class>
+	class TNodeTemplate {
+	private:
+	public:
+
+	};
+
+	class TNodeCore:public std::enable_shared_from_this<TNodeCore>{
+	protected:
+	public:
+		std::string description; //説明
+		ImVec2 size_; //大きさ
+		ImVec2 pos_; //座標
+		std::string title_; //ノード名前
+		uint64_t ID_;
+		Node_TYPE type_;
+		std::vector<std::shared_ptr<TSocketCore>> socketsInput;
+		std::vector<std::shared_ptr<TSocketCore>> socketsOutput;
+		TNodeCore() = delete;
+		TNodeCore(Node_TYPE _type, std::string _title, vec2 _pos, vec2 _size) :
+			ID_(uint64_t(this)),
+			type_(_type),
+			title_(_title),
+			description(""),
+			pos_(_pos),
+			size_(_size)
+		{};
+
+		void SetDesciption(std::string _description);
+
+		void AddOutPutSocket(Socket_TYPE _scktType);
+		void AddInputSocket(Socket_TYPE _scktType);
+	};
+
+	//選択肢用ノード。
+	class TNodeBranch :public TNodeCore {
+	private:
+	public:
+		TNodeBranch(std::string _title, vec2 _pos,vec2 _size=vec2(100,50)) :
+			TNodeCore(Node_TYPE::BRANCH, _title, _pos, _size) {
+			this->AddInputSocket(Socket_TYPE::TIMELINE);
+			this->AddOutPutSocket(Socket_TYPE::TIMELINE);
+			this->AddOutPutSocket(Socket_TYPE::TIMELINE);
+		};
+		~TNodeBranch() = default;
+	};
+
+	class TNodeAnimation :public TNodeCore {
+	public:
+
+	};
+
+	class TNodeEvent :TNodeCore {
+
+	};
+
 }
 
